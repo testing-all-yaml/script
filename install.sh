4018843d48705dda9f435e6498fb808390917b29
K.dohnwana <86168117+kingsleydoh@users.noreply.github.com>
2024-04-21 23:27:43 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,20 @@ sudo apt update
 sudo apt install -y apache2
 
 # Enable Azure AD login
-sudo waagent -enable-aad
+# Install the WALinuxAgent package if not already installed
+sudo apt install walinuxagent -y
+
+# Enable Azure AD login
+sudo sed -i 's/# Extension to enable AAD login/Extension to enable AAD login\n    EnableSSHKeyBasedAuthentication n/g' /etc/waagent.conf
+sudo sed -i 's/# ResourceDisk.Format=ext4/ResourceDisk.Format=ext4/g' /etc/waagent.conf
+sudo sed -i 's/# ResourceDisk.Filesystem=ext4/ResourceDisk.Filesystem=ext4/g' /etc/waagent.conf
+sudo sed -i 's/# ResourceDisk.MountPoint=\/mnt\/resource/ResourceDisk.MountPoint=\/mnt\/resource/g' /etc/waagent.conf
+
+# Restart the WAagent service
+sudo systemctl restart walinuxagent
 
 # Disable root login
-sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+#sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 
 # Restart SSH service to apply changes
 sudo systemctl restart sshd
